96c4d42fcefeec67021e06c7db4a9ab7773f19a6
TheMaximuzz <maks.natorin@gmail.com>
2025-04-16 21:58:45 +0500

    new localization tests

---
+++ src/test/java/robots/gui/LogWindowTest.java
@@ -0,0 +1,28 @@
+package robots.gui;
+
+import org.junit.jupiter.api.Test;
+import robots.log.Logger;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LogWindowTest {
+
+    @Test
+    public void testGetTitleKey() {
+        LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
+        assertEquals("logWindowTitle", logWindow.getTitleKey());
+    }
+
+    @Test
+    public void testTitleLocalization() {
+        LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
+        String key = logWindow.getTitleKey();
+
+        ResourceBundle enBundle = ResourceBundle.getBundle("messages", Locale.US);
+        assertEquals("Work Log", enBundle.getString(key));
+
+        ResourceBundle ruBundle = ResourceBundle.getBundle("messages", new Locale("ru", "RU"));
+        assertEquals("Протокол работы", ruBundle.getString(key));
+    }
+}
